97e22e6da264433184d4905cd42be759ddddb886
Daniyal Saleem <daniyalsaleem.d@gmail.com>
2021-04-13 04:59:57 +0500

    refactor

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,27 @@
 import { useState } from "react";
 import { useHistory } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
-  const [author, setAuthor] = useState('mario');
+  const [isValid, setIsValid] = useState(false);
+  const [author, setAuthor] = useState(undefined);
   const history = useHistory();
+
+  const { data: blogs } = useFetch('http://localhost:8000/blogs');
+  const authors = Array.from(new Set(blogs && blogs.map((v) => v.author)));
+  authors.unshift("Select Author");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    
+    if(!isValid)
+    {
+      alert('Please provide valid inputs');
+      return;
+    }
+
     const blog = { title, body, author };
 
     fetch('http://localhost:8000/blogs/', {
@@ -16,7 +29,6 @@ const Create = () => {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(blog)
     }).then(() => {
-      // history.go(-1);
       history.push('/');
     })
   }
@@ -30,23 +42,29 @@ const Create = () => {
           type="text" 
           required 
           value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={(e) => setTitle(e.target.value.trim())}
         />
         <label>Blog body:</label>
         <textarea
           required
           value={body}
-          onChange={(e) => setBody(e.target.value)}
+          onChange={(e) => setBody(e.target.value.trim())}
         ></textarea>
         <label>Blog author:</label>
         <select
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
+          required={true}
+          placeholder="Select"
+          value={author}  
+          onChange={(e) => {
+            setAuthor(e.target.value );
+            setIsValid(e.target.value === "Select Author" ? false : true);
+          }
+        }
         >
-          <option value="mario">mario</option>
-          <option value="yoshi">yoshi</option>
+        {authors && authors.map(x => <option key={x} value={x}> {x} </option> )}
+
         </select>
-        <button>Add Blog</button>
+        <button >Add Blog</button>
       </form>
     </div>
   );
